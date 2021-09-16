54363bbccb0bb840d13b219c7b0c50d0249004a1
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-09-16 14:33:54 -0300

    teste requisito 1 ao 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: true } },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { nome: 1, _id: 0 },
+);
