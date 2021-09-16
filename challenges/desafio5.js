2e4a2949882eefc1156b6b2cca06fa690f4f9e4c
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-09-16 14:59:03 -0300

    req5 ok

---
+++ challenges/desafio5.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+  { nome: { $not: { $eq: "McChicken" } } },
+  {
+    $addToSet: {
+      ingredientes: "ketchup",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1,
+  },
+);
