3b17438e6e043a1a2d2e55e174ab481cfd128859
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-16 14:53:22 -0300

    desafio 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,5 +1,4 @@
 // Crie uma query que adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken, garantindo que não haja duplicidade nos ingredientes.
-
 db.produtos.updateMany(
   {
     nome: { $ne: "McChicken" },
