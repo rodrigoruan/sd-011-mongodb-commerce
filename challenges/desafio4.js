ef71d7b916563e2c0b0a901ae81f04a575d9a099
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-09-16 14:43:40 -0300

    Desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,4 @@
+db.produtos.updateMany({ nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: { $type: "date" } } });
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: 1, _id: 0 });
