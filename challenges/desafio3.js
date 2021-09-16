008eb05f82ba4108f566a47b4c98a0d05bc77978
jhenriquemargrafm <jh.margraf@gmail.com>
2021-09-16 19:50:09 -0300

    Requisito 3 - uso de tag

---
+++ challenges/desafio3.js
@@ -0,0 +1,20 @@
+// Query 1 - Adicionar avaliação em todos os documentos
+db.produtos.updateMany(
+  {}, 
+  { $set: { avaliacao: NumberInt(0) } },
+  { upsert: true },
+);
+
+// Query 2 - Incrementar o valor do campo avaliação em 5 em todos os sanduíches de carne do tipo bovino
+db.produtos.updateMany(
+  { tags: { $in: ["bovino"] } }, 
+  { $set: { avaliacao: 5 } },
+);
+
+// Query 3 - Incrementar o valor do campo avaliacao em 3 em todos os sanduíches de ave
+db.produtos.updateMany(
+  { tags: { $in: ["ave"] } }, 
+  { $set: { avaliacao: 3 } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
