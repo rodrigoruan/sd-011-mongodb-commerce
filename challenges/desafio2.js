55e26163139d6b7637ca51161d0fbc57010d7cdc
IsaacMagno <isaacmagno98@gmail.com>
2021-09-16 15:46:49 -0300

    desafio2cor

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,2 +1,2 @@
 db.produtos.updateMany({}, { $set: { valorUnitario: NumberDecimal("0.00") } });
-db.produtos.find({ criadoPor: "Ronald McDonald" }, { nome: 1, valorUnitario: 1, _id: 0 });
+db.produtos.find({}, { nome: 1, valorUnitario: 1, _id: 0 });
---
+++ challenges/desafio3.js
@@ -0,0 +1,1 @@
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
