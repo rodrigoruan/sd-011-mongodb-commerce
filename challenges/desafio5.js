62bb9e5aed305eeee5cc4b4ae91e4309478eca31
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-16 15:32:22 -0300

    Correcao lint

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,9 +1,9 @@
 db.produtos.updateMany(
-  {nome: {$not:/McChicken/}},
-  {$addToSet: {ingredientes: "ketchup"}}
+  { nome: { $not: /McChicken/ } },
+  { $addToSet: { ingredientes: "ketchup" } },
   );
 
 db.produtos.find(
 {},
-{nome: 1, ingredientes: 1, _id: 0}
+{ nome: 1, ingredientes: 1, _id: 0 },
 );
