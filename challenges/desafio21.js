1b6ef69119e85af536e94fddb2e0dec13a345702
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-16 16:23:32 -0300

    Create desafio21.js

---
+++ challenges/desafio21.js
@@ -0,0 +1,2 @@
+// Retorne o nome dos sanduíches em que o número de curtidas é maior que o número de sanduíches vendidos.
+db.produtos.find({ $gt: ["$curtidas", "$vendidos"] }, { _id: 0, nome: 1 });
