b057be9903e32ed1cbae61e6d6e0bfc864f97126
IsaacMagno <isaacmagno98@gmail.com>
2021-09-16 16:19:42 -0300

    cor4.2

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,5 +1,5 @@
 db.produtos.updateOne(
   { nome: "Big Mac" },
-  { $currentDate: { ultimaModificacao: { $type: "date" } } }
+  { $currentDate: { ultimaModificacao: { $type: "date" } } },
   );
 db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
