9dc15e1603b7737ea4fedb08561d288e3457c253
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-16 15:36:56 -0300

    Update desafio12.js

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,11 +1,11 @@
 // Crie uma query que faça em todos os documentos a ordenação dos elementos do array valoresNutricionais pelo campo percentual de forma descendente. Dica: mesmo sem adicionar nenhum novo elemento, para essa operação é necessário utilizar também o modificador $each.
 db.produtos.updateMany(
   {},
-  {
+  { 
     $push: {
       valoresNutricionais: {
         $each: [],
-        $sort: { $percentual: -1 },
+        $sort: { percentual: -1 },
       },
     },
   },
