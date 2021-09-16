3bc852c664ec384f1f4f4bb0e3570d2959d2e007
Thyago Pessoa <flash_thyago@hotmail.com>
2021-09-16 14:47:36 -0300

    Desafio 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+  { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+  {
+    $push: {
+      ingredientes: "bacon",
+    },
+  },
+);
+
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
