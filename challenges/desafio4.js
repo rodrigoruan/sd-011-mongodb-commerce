92e40faa624321459b86d73b2d6c43e64471ce05
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-09-16 15:19:31 -0300

    realiza desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $currentDate: {
+    ultimaModificacao: true,
+  } },
+);
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, {
+  _id: 0,
+  nome: 1,
+});
