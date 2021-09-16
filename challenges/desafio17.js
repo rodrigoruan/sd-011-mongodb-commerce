d67e3aa6a85218a90dc4c575c31441dc20914744
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-09-16 16:43:45 -0300

    req17 ok

---
+++ challenges/desafio17.js
@@ -0,0 +1,12 @@
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
+);
+
+db.produtos.count(
+  {
+    $text: {
+      $search: "frango hamburguer",
+    },
+  },
+);
