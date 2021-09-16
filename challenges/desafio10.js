21b39c409016cdfcb18ac8505150111aa0da0652
marlon307 <marlon-ramosb@hotmail.com>
2021-09-16 17:38:58 -0300

    Requesito 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,32 @@
+db.produtos.updateMany({}, {
+  $set: {
+    vendasPorDia: [0, 0, 0, 0, 0, 0, 0],
+  },
+});
+
+db.produtos.updateMany({
+  nome: "Big Mac",
+}, {
+  $set: {
+    "vendasPorDia.3": 60,
+  },
+});
+
+db.produtos.updateMany({
+  ingredientes: {
+    $elemMatch: {
+      $gt: "bovino",
+      $gt: "pão",
+    },
+  },
+}, {
+  $set: {
+    "vendasPorDia.6": 120,
+  },
+});
+
+db.produtos.find({}, {
+  _id: 0,
+  nome: 1,
+  vendasPorDia: 1,
+});
