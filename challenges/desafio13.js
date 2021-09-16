168c746b2a046a824903eb68798ef48ef85c1650
rodolforezende <rodolforrcont@gmail.com>
2021-09-16 15:49:03 -0300

    Desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,17 @@
+use("commerce");
+db.produtos.updateMany(
+    {
+        valoresNutricionais: {
+            $elemMatch: {
+                tipo: { $eq: "sódio"},
+                percentual: { $gte: 40 },
+            },
+        },
+    },
+    { $push: { tags: "muito sódio" } },
+);
+
+db.produtos.find(
+    { },
+    { nome: 1, tags: 1, _id: 0 },
+);
