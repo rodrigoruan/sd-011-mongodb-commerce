f6c455a2b1f847e87e841ee9d23d6c23e6e53915
menezesrenan7 <renan_menezes07@hotmail.com>
2021-09-16 18:48:08 -0300

    Ajustes do ESLint.

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,8 +1,9 @@
 db.produtos.updateMany(
   { 
-    nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } 
+    nome: { $in: ["Big Mac", "Quarteirão com Queijo"] },
   }, 
   {
-    $push: { ingredientes: "bacon" }
-  });
+    $push: { ingredientes: "bacon" },
+  }
+  );
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
