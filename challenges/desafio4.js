d29144f7ad4cf43c94f905b3e6399a89d3f0b8d3
Leandro Lopes <leandro_carlos@uol.com.br>
2021-09-16 17:32:10 -0300

    desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,10 @@
+use("commerce");
+db.produtos.updateOne(
+    { nome: "Big Mac" },
+    { $set: { ultimaModificacao: new Date() } },
+);
+
+db.produtos.find(
+    { ultimaModificacao: { $exists: true } },
+    { nome: 1, _id: 0 }, 
+);
