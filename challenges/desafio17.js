b8685d4f87be7f23adada075d978a42875abfa67
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-09-16 16:35:34 -0400

    Desafio 17, feito.

---
+++ challenges/desafio17.js
@@ -0,0 +1,14 @@
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
+);
+
+db.produtos.count(
+  {
+    $text: {
+      $search: "frango hamburguer",
+    },
+  },
+);
+
+// db.produtos.getIndexes();
