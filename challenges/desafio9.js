c145b472aee32adca4b374c3699f5c68d1180965
Filizzola <juliofilizzola@hotmail.com>
2021-09-16 14:37:46 -0300

    desafio 8

---
+++ challenges/desafio9.js
@@ -0,0 +1,10 @@
+// use('commerce');
+db.produtos.updateOne(
+  { nome: "Cheddar McMelt" },
+  { $pop: { ingredientes: 1 } },
+);
+
+db.produtos.find(
+  { },
+  { nome: 1, ingredientes: 1, _id: 0 },
+);
