f27912b64e229d88ca629ed64c081ac4e6716e92
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-09-16 14:58:04 -0300

    desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({}, { $push: { criadoPor: "Ronald McDonalds" } });
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonalds" } });
 db.produtos.find({}, { _id: 0, nome: 1, criadoPor: 1 }); 
