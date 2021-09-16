116153ac0d1f99c2da7d305736d6ce3274701433
Thiagofox <thiagofox00@gmail.com>
2021-09-16 16:01:47 -0300

    Desafios 4 finalizado

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -8,4 +8,17 @@ Para isso, escreva no arquivo desafio4.js duas queries, nesta ordem:
 2- Crie uma query que retorne o nome de todos os documentos em que o campo ultimaModificacao existe.
 
 */
+
+// use("commerce");
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: { $type: "date" } } },
+);
+
+// use("commerce");
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: 0, nome: 1 },
+);
+
 //= ==========================================================================
