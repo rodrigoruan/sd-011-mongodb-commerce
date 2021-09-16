2a383f8e9aef7709b302b06a9a149e01032606f0
rodolforezende <rodolforrcont@gmail.com>
2021-09-16 16:23:07 -0300

    Desafio 18

---
+++ challenges/desafio18.js
@@ -0,0 +1,9 @@
+db.produtos.createIndex(
+    { descricao: "text" },
+    { default_language: "portuguese" },
+);
+// https://docs.mongodb.com/manual/tutorial/create-indexes-to-support-queries/
+
+db.produtos.count(
+    { $text: { $search: "feito com" } },
+);
