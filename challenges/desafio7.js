f1f469cbf361d07b9b57d7386e352a0db2315c9d
marcoswarmling <marcoswarmling15@gmail.com>
2021-09-16 15:34:28 -0300

    desafio 8

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
     {},
-    { $pull: { ingredientes: "cebola" } }
+    { $pull: { ingredientes: "cebola" } },
 );
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
