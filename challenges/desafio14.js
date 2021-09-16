616fc281512608eb66e87998688a43315fa9d4b4
marcoswarmling <marcoswarmling15@gmail.com>
2021-09-16 16:14:37 -0300

    desafio 16

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
-    {"valoresNutricionais.2.percentual": {$lt: 40, $gt: 20}},
-    { $push: {tags: "contém sódio"} }
+    { "valoresNutricionais.2.percentual": { $lt: 40, $gt: 20 } },
+    { $push: { tags: "contém sódio" } },
 );
 db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,1 +1,1 @@
-db.produtos.count({nome:{$regex: /Mc/i}});
+db.produtos.count({ nome: { $regex: /Mc/i } });
