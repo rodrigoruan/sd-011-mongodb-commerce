476ebf26017cde8299155bc8d35700f607594a9a
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-09-16 17:14:46 -0300

    Requisitos realizados ate o 17

---
+++ challenges/desafio15.js
@@ -0,0 +1,2 @@
+/* 15 - Conte quantos produtos contêm Mc no nome, sem considerar letras maiúsculas ou minúsculas. */
+db.produtos.count({ nome: /mc/i });
---
+++ challenges/desafio16.js
@@ -0,0 +1,2 @@
+/* 16 - Conte quantos produtos têm 4 ingredientes. */
+db.produtos.count({ ingredientes: { $size: 4 } });
