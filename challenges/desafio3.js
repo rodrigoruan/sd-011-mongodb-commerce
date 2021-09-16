2c3ce04ad823b68c872f5a56a0b332ace563430d
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-16 16:08:15 -0300

    desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,29 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: { avaliacao: NumberInt("0") },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["bovino"] } },
+  {
+    $inc: {
+      avaliacao: 5,
+    },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["ave"] } },
+  {
+    $inc: {
+      avaliacao: 3,
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, _id: 0, avaliacao: 1 },
+);
