2c9a7691696caacba1396631aa41becbf18cb82a
KaioHPs <kaiohpsalternativo@gmail.com>
2021-09-16 16:33:21 -0300

    Requisitos 7 a 12 e Correçao do Requisito 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,4 +1,4 @@
 db.produtos.updateMany({ valorUnitario: { $exists: false } },
-  { $set: { NumberDecimal: NumberDecimal("0.00") } });
+  { $set: { valorUnitario: NumberDecimal("0.00") } });
 
-db.produtos.find({}, { _id: 0, nome: 1, NumberDecimal: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, valorUnitario: 1 });
