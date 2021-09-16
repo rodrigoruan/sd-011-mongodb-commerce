d8442de2a0bb8b3166b6578745ee96e78b18a258
Víctor Faria <vitilevi@hotmail.com>
2021-09-16 14:43:51 -0300

    Feito requisito 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,15 @@
+db.produtos.updateOne({
+  nome: "Big Mac",
+}, {
+  $currentDate: {
+    ultimaModificacao: true,
+  },
+});
+db.produtos.find({
+  ultimaModificacao: {
+    $exists: true,
+  },
+}, {
+  _id: 0,
+  nome: 1,
+});
