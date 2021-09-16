dc7b341b5499532ae372f9ffdcfa9dba58d5f579
jamneto <netomoura22@gmail.com>
2021-09-16 16:13:37 -0300

    corrigindo req 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,6 +1,6 @@
 db.produtos.updateOne(
   { nome: "Big Mac" },
-  { $currentDate: { ultimaModificacao: true } },
+  { $set: { ultimaModificacao: Date() } },
 );
 db.produtos.find({ ultimaModificacao: { $exists: true } },
   { _id: 0, nome: 1, ultimaModificacao: 1 });
