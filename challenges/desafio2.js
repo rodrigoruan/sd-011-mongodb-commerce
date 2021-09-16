5b69425403d6c5c907fc02e8e1d8ca3c149f71d0
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-16 14:56:15 -0300

    Iniciando projeto

---
+++ challenges/desafio2.js
@@ -0,0 +1,6 @@
+// 2 - Inclua o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
+// Para isso, escreva no arquivo desafio2.js duas queries, nesta ordem:
+
+// Crie uma query que adicione o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
+
+// Crie uma query que retorne o nome e valorUnitario de todos os produtos.
