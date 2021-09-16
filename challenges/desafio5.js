283dd067cec58d9ac03029ba6f875a0c031adc5a
Jean Esteves <jean.esteves2001@gmail.com>
2021-09-16 19:03:35 -0300

    fix: desafio 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,3 +1,3 @@
-db.produtos.updateMany({ nome: { $not: /McChicken/ } }, { $push: { ingredientes: "ketchup" } });
+db.produtos.updateMany({ nome: { $not: /McChicken/ } }, { $addToSet: { ingredientes: "ketchup" } });
 
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
