b88690071dfd4637c9073fe5f6a0eace7218d086
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-16 14:56:00 -0300

    Create desafio7.js

---
+++ challenges/desafio7.js
@@ -0,0 +1,12 @@
+// Crie uma query que faça a remoção do item cebola em todos os sanduíches.
+db.produtos.updateMany(
+  {},
+  {
+    $unset: {
+      ingredientes: "cebola",
+    },
+  },
+);
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
