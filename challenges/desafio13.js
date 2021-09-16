2093cf4ae8026bb25174f0aefbf344c917321d4f
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-16 11:49:16 -0300

    FEAT: Desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany(
+  { valoresNutricionais: {
+    $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } },
+  } },
+  { $push: { tags: "muito sódio" } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, _id: 0, tags: 1 },
+);
