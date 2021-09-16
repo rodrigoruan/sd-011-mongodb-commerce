6768abb476dafd383c8a31654c2012fc7c2f42b7
theusseveen22 <mateus.krauss@gmail.com>
2021-09-16 19:45:11 -0300

    Adiciona requisitos 1,2,3,4

---
+++ challenges/desafio3.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany({}, 
+    { $set: { avaliacao: NumberInt("0") } });
+db.produtos.updateMany(
+    { tags: "bovino" },
+    { $set: { avaliacao: 5 } },
+);
+db.produtos.updateMany(
+    { tags: "ave" },
+    { $set: { avaliacao: 3 } },
+);
+db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });
---
+++ challenges/desafio4.js
@@ -0,0 +1,6 @@
+db.produtos.updateMany(
+    { nome: "Big Mac" },
+    { $set: { ultimaModificacao: new Date() } },
+);
+db.produtos.find({ ultimaModificacao: { $exists: true } },
+    { nome: 1, _id: 0 });
