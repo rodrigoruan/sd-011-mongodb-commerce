fe65190e71f9cad153687297395c3cae1be396ef
Marconi <marconieng@gmail.com>
2021-09-16 15:43:20 -0300

    Adiciona o operador upsert a querie

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,3 +1,7 @@
-db.produtos.updateMany({}, { $push: { criadoPor: "Ronald McDonald" } });
+db.produtos.updateMany(
+  {},
+  { $push: { criadoPor: "Ronald McDonald" } },
+  { upsert: true }
+);
 
 db.produtos.find({}, { _id: 0, nome: 1, criadoPor: 1 });
