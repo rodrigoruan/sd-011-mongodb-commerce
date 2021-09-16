d8984f2ee8c6473865f4a55fabab373399e09ed5
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-09-16 16:11:55 -0400

    Desafio 4, feito.

---
+++ challenges/desafio4.js
@@ -0,0 +1,16 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $currentDate: { ultimaModificacao: true },
+  },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao: { $exists: 1 },
+  },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
