366c0adb42b654450c40c79148221b139551e8fb
Daniel Leon <daniel.radialista@gmail.com>
2021-09-16 17:02:45 -0300

    desafio 10 pronto

---
+++ challenges/desafio10.js
@@ -0,0 +1,21 @@
+db.produtos.updateMany(
+  {},
+  { 
+    $push: { 
+    vendasPorDia: {
+      $each: [0, 0, 0, 0, 0, 0, 0],
+    },
+  } },
+);
+
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $inc: { "vendasPorDia.3": 60 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["bovino", "pão"] } },
+  { $inc: { "vendasPorDia.6": 120 } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
