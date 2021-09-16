a13a17fcdf31a8fd8731fba209a0329931b36826
Jessica Arruda <sicarruda@gmail.com>
2021-09-16 16:05:50 -0300

    exercicios 5 e 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany(
+  { $or: [
+    { nome: { $eq: "Big Mac" } },
+    { nome: { $eq: "Quarteirão com Queijo" } },
+    ] },
+    { $addToSet: { ingredientes: "bacon" } },
+);
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
