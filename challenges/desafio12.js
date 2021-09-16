be59b080de09dc9ce70d9ea792b0831685f89405
Jean Esteves <jean.esteves2001@gmail.com>
2021-09-16 19:17:42 -0300

    fix: desafio12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -3,4 +3,4 @@ db.produtos.updateMany(
   { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } },
 );
 
-db.produtos.find({}, { nome: 1, valoresNutricionais: 1, _id: 0});
+db.produtos.find({}, { nome: 1, valoresNutricionais: 1, _id: 0 });
