71dd87ab48a14adb555ea6834575b2eaa7d64890
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-09-16 16:22:06 -0300

    req14 ok

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -3,13 +3,13 @@ db.produtos.updateMany(
     valoresNutricionais: {
       $elemMatch: {
         tipo: "sódio",
-        percentual: { $gte: 40 },
+        percentual: { $gt: 20, $lt: 40 },
       },
     },
   },
   {
     $push: {
-      tags: "muito sódio",
+      tags: "contém sódio",
     },
   },
 );
