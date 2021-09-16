00c7a705b0d187804a4493a41899fdc424df881f
Marconi <marconieng@gmail.com>
2021-09-16 15:45:19 -0300

    Adiciona virgula

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,7 +1,7 @@
 db.produtos.updateMany(
   {},
   { $push: { criadoPor: "Ronald McDonald" } },
-  { upsert: true }
+  { upsert: true },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, criadoPor: 1 });
