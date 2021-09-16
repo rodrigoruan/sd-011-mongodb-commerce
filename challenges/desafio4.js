875868b02d52dd4a0a6a3104a0bf97a96b32d7d7
Daniel Leon <daniel.radialista@gmail.com>
2021-09-16 15:26:18 -0300

    desafio 4 pronto e rodando nos testes locais

---
+++ challenges/desafio4.js
@@ -0,0 +1,10 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $currentDate: {
+      ultimaModificacao: { $type: "date" },
+    },
+  },
+);
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
