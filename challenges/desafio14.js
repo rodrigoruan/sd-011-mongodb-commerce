819139daea88ec43f220ec81535eb3b654da5e0f
marcoswarmling <marcoswarmling15@gmail.com>
2021-09-16 16:06:34 -0300

    desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,5 @@
+db.produtos.updateMany(
+    {"valoresNutricionais.2.percentual": {$lt: 40, $gt: 20}},
+    { $push: {tags: "contém sódio"} }
+);
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
