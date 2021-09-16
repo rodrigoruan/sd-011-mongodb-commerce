2e26829af08435429713ed137ffdb3241f6843aa
Jessica Arruda <sicarruda@gmail.com>
2021-09-16 18:41:34 -0300

    exercicio 17 e correção dos exercicios 13 e 14

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,5 +1,8 @@
 db.produtos.updateMany(
-  { "valoresNutricionais.2.quantidade": { $gte: 40 } }, 
-  { $push: { tags: "muito sódio" } },
-  );
+  { $and: [
+    { "valoresNutricionais.tipo": { $eq: "sódio" } }, 
+    { "valoresNutricionais.quantidade": { $gte: 40 } },
+  ] },
+     { $push: { tags: "muito sódio" } },
+);
   db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,7 +1,8 @@
 db.produtos.updateMany(
   { $and: [ 
-    { "valoresNutricionais.2.quantidade": { $gt: 20 } },
-    { "valoresNutricionais.2.quantidade": { $lt: 40 } },
+    { "valoresNutricionais.tipo": { $eq: "sódio" } },
+    { "valoresNutricionais.quantidade": { $gt: 20 } },
+    { "valoresNutricionais.quantidade": { $lt: 40 } },
   ] },
   { $push: { tags: "contém sódio" } },
 );
