3f243819b2c9d347cd07ec81fc87999d9d3366c8
IsaacMagno <isaacmagno98@gmail.com>
2021-09-16 15:24:38 -0300

    desafio1cor

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,2 +1,2 @@
 db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
-db.produtos.find({ criadoPor:"Ronald McDonald" }, { nome: 1, criadoPor: 1, _id: 0 });
+db.produtos.find({ criadoPor: "Ronald McDonald" }, { nome: 1, criadoPor: 1, _id: 0 });
