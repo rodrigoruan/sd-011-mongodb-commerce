c6f8105a225a768b3e6b72e91c0da45bea7f6d69
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-16 16:01:24 -0300

    desafio 2

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,11 +1,11 @@
 db.produtos.updateMany(
   {},
   { $set:
-    { criadoPor: "Ronald McDonald"}
-  }
+    { criadoPor: "Ronald McDonald" },
+  },
 );
 
 db.produtos.find(
   {},
-  { nome: 1, _id: 0, criadoPor: 1}
+  { nome: 1, _id: 0, criadoPor: 1 },
 );
