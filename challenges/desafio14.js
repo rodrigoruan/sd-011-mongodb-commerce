5999bd643ba3621b4e95ae1998fd10ac82792d74
LeoFuna <leo.funa@gmail.com>
2021-09-16 11:26:20 -0300

    Requisito 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({ "valoresNutricionais.2.percentual": { $gt: 20, $lt: 40 } }, 
+{ $addToSet: { tags: "contém sódio" } });
+db.produtos.find({ }, { nome: 1, tags: 1, _id: 0 });
