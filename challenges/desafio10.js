f9726cfaa67447e296f0e725baaf05fa947766a1
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-16 15:18:01 -0300

    add: requisito 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,35 @@
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      vendasPorDia: {
+        $each: [
+          0, 0, 0, 0, 0, 0, 0,
+        ],
+      },
+    },
+  },
+);
+
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  {
+    $inc: {
+      "vendasPorDia.3": 60,
+    },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["pão", "bovino"] } },
+  {
+    $inc: {
+      "vendasPorDia.6": 120,
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, vendasPorDia: 1 },
+);
