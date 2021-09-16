f71715a182c8cc33697d3a9dadf86afe660742bf
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-09-16 16:24:15 -0400

    Desafio 10, feito.

---
+++ challenges/desafio10.js
@@ -0,0 +1,39 @@
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      vendasPorDia: {
+        $each: [0, 0, 0, 0, 0, 0, 0],
+      },
+    },
+  },
+);
+
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $set: {
+      "vendasPorDia.3": 60,
+    },
+  },
+);
+
+db.produtos.updateMany(
+  {
+    tags: { $in: ["bovino", "pão"] },
+  },
+  {
+    $set: {
+      "vendasPorDia.6": 120,
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    vendasPorDia: 1,
+  },
+);
