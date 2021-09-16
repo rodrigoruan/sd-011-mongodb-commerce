f4e1b1c3a5072e06f07c7c5a55b6e4be5510236f
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-09-16 18:10:15 -0300

    realiza desafio 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,16 @@
+db.produtos.createIndex(
+  { 
+    descricao: "text",
+  }, 
+  { 
+    default_language: "portuguese",
+  },
+);
+
+db.produtos.find(
+  {
+    $text: {
+      $search: "frango hamburguer",
+    },
+  },
+).count();
