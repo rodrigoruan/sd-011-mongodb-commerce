5cb48053052699e545b92adfc3d9a94932db2e96
jhenriquemargrafm <jh.margraf@gmail.com>
2021-09-16 19:50:25 -0300

    Requisito 3 - uso de tag

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -17,4 +17,7 @@ db.produtos.updateMany(
   { $set: { avaliacao: 3 } },
 );
 
-db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
+db.produtos.find(
+  {}, 
+  { _id: 0, nome: 1, avaliacao: 1 },
+);
