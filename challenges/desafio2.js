93c8ef4a3bc8b2ac2da1a9f5d5b2e1073ecfbecc
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-16 14:49:55 -0300

    arruma requisito 02

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,8 +1,8 @@
 db.produtos.updateMany({
-  valorUnitario: { $exists: false }
+  valorUnitario: { $exists: false },
 },
 {
-  $set: { valorUnitario: NumberDecimal("0.00") }
+  $set: { valorUnitario: NumberDecimal("0.00") },
 });
 
 db.produtos.find({}, { _id: 0, nome: 1, valorUnitario: 1 });
