bb548f15a1a1f63d2c999c69f1f6ee388a5fc799
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-16 17:26:56 -0300

    Ex de 11 e 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -2,5 +2,10 @@
 // Para isso, escreva no arquivo desafio12.js duas queries, nesta ordem:
 
 // Crie uma query que faça em todos os documentos a ordenação dos elementos do array valoresNutricionais pelo campo percentual de forma descendente. Dica: mesmo sem adicionar nenhum novo elemento, para essa operação é necessário utilizar também o modificador $each.
+db.produtos.updateMany(
+  {},
+  { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } },
+);
 
 // Crie uma query que retorne o nome e valoresNutricionais de todos os documentos.
+db.produtos.find({}, { nome: true, valoresNutricionais: true, _id: false });
