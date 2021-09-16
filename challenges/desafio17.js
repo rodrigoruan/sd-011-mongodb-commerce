d3660f066013ea27d13871fdfb65382c21f90ff1
rodolforezende <rodolforrcont@gmail.com>
2021-09-16 16:18:34 -0300

    Desafio 17 - Corrigido

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -2,8 +2,8 @@ db.produtos.createIndex(
     { descricao: "text" },
     { default_language: "portuguese" },
 );
-//https://docs.mongodb.com/manual/tutorial/create-indexes-to-support-queries/
+// https://docs.mongodb.com/manual/tutorial/create-indexes-to-support-queries/
 
 db.produtos.count(
     { $text: { $search: "frango hamburguer" } },
-)
+);
