f8c45113eafafe349a29c059b1094ab19319bf6e
heyynat <natalifplima@gmail.com>
2021-09-16 16:38:41 -0400

    Desafio 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany(
+    { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+    { $push: 
+        { ingredientes:
+          "bacon",
+        },   
+      },
+    { upsert: true },
+);
+
+db.produtos.find({}, { nome: 1, _id: 0, ingredientes: 1 });
