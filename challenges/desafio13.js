8dcdd13ea520b05943a7083eb092b3fcbab206f6
Hamander <1hamander@gmail.com>
2021-09-16 15:39:42 -0300

    challenges 9 to 14

---
+++ challenges/desafio13.js
@@ -0,0 +1,24 @@
+db.produtos.updateMany(
+  { 
+  valoresNutricionais: { 
+    $elemMatch: {
+      tipo: { $eq: "sódio" },
+      percentual: { $gte: 40 },
+    },
+  }, 
+},
+{ 
+  $addToSet: {
+    tags: "muito sódio",
+  },
+},
+);
+
+db.produtos.find(
+{ }, 
+{
+  _id: 0, 
+  nome: 1,
+  tags: 1,
+},
+);
