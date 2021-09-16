e35576cc48c57df8596d3ee022c81621b6ce7346
heyynat <natalifplima@gmail.com>
2021-09-16 14:17:56 -0400

    Desafio 1 - Correto

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({}, { $push: { criadoPor: "Ronald McDonald" } }, { upsert: true });
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } }, { upsert: true });
 db.produtos.find({}, { nome: 1, _id: 0, criadoPor: 1 });
