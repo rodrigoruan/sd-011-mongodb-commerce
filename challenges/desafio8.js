bb5060efe3657a64b202a9567fb44d58cfa1661f
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-16 14:59:21 -0300

    Create desafio8.js

---
+++ challenges/desafio8.js
@@ -0,0 +1,12 @@
+// Crie uma query que faça a remoção do primeiro ingrediente no sanduíche Quarteirão com Queijo.
+db.produtos.updateMany(
+  {
+    nome: "Quarteirão com Queijo",
+  },
+  {
+    $pop: { ingredientes: - 1 },
+  },
+);
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
