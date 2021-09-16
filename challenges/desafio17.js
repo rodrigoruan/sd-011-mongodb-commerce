a89fccb1e2ab707d722bba30d143472c73cb4083
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-16 18:46:46 -0300

    desafio 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,1 +1,7 @@
-db.produtos.createIndex({ descricao: "text" });
+db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
+
+db.produtos.count(
+  { $text: {
+    $search: "frango hamburguer",
+  } },
+);
