0b7f0d52650da66d350c7a7d8e4c328101ffa1bd
juliofilizzola <juliofilizzola@Hotmail.com>
2021-09-16 18:41:43 -0300

    fix: Desafio 13 deletando Console.log

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,5 +1,3 @@
-// use('commerce');
-
 db.produtos.updateMany(
     { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
     { $addToSet: { tags: "muito sódio" } },
