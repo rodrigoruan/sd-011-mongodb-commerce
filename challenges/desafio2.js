6909f0f72c398e227f6ae80d3e7abdbfa1873642
jhenriquemargrafm <jh.margraf@gmail.com>
2021-09-16 19:40:41 -0300

    Requisito 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,11 @@
+// Query 1 - Adicionar valorUnitário nos campos que não existem
+db.produtos.updateMany(
+  { valorUnitario: { $exists: false } },
+  { $set: { valorUnitario: NumberDecimal("0.00") } },
+  { upsert: true },
+);
+
+db.produtos.find(
+  {}, 
+  { _id: 0, nome: 1, valorUnitario: 1 },
+);
