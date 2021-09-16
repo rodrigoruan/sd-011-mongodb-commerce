b1c04944f42b9c0b06694333b106329934650661
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-09-16 16:54:15 -0300

    piada

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -10,6 +10,6 @@ db.produtos.updateMany(
       $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } },
     },
   },
-  { $push: { tags: "muito sódio" } }
+  { $push: { tags: "muito sódio" } },
 );
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
