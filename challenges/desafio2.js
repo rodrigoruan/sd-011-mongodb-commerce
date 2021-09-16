690bc4b4ab378a7ffe8adea0abfebf5612794014
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-16 15:39:58 -0300

    Ex de 1 a 4

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -2,5 +2,7 @@
 // Para isso, escreva no arquivo desafio2.js duas queries, nesta ordem:
 
 // Crie uma query que adicione o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
+db.produtos.updateMany({}, {$set: {valorUnitario: NumberDecimal("0.00")}});
 
 // Crie uma query que retorne o nome e valorUnitario de todos os produtos.
+db.produtos.find({}, {nome: true, valorUnitario: true, _id: false});
