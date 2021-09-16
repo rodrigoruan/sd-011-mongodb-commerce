b216a84c3c0c70ff614239bf574a612efdad7804
anaclarabck <ackyotoku@gmail.com>
2021-09-16 15:55:04 -0300

    [ADD] challenges 1~5

---
+++ challenges/desafio2.js
@@ -0,0 +1,5 @@
+db.produtos.updateMany(
+  {},
+  { $set: { valorUnitario: NumberDecimal("0.00") } },
+);
+db.produtos.find({}, { _id: 0, nome: 1, valorUnitario: 1 });
---
+++ challenges/desafio5.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany(
+  { nome: { $ne: "McChicken" } },
+  { $push: {
+    ingredientes: {
+      $each: ["ketchup"],
+    },
+  } },
+);
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
