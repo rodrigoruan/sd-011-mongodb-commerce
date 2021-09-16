1eafe1836359813f77c8cf0c05e20c77d4056bb3
KaioHPs <kaiohpsalternativo@gmail.com>
2021-09-16 18:28:27 -0300

    Correçao Requisito 19

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,3 +1,3 @@
-db.produtos.updateMany({}, { $rename: { "descricao": "descricaoSite" } });
+db.produtos.updateMany({}, { $rename: { descricao: "descricaoSite" } });
 
 db.produtos.find({}, { _id: 0, nome: 1, descricao: 1, descricaoSite: 1 });
