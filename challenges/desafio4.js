397f773317542bddfbb08552fd9923915a369fb5
Diogo Augusto <diogotrescastro@gmai.com>
2021-09-16 15:13:34 -0300

    desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,6 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: { $type: "date" } } }
+);
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
