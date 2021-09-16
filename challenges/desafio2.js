814183470d3f89f7965515f61ab021fef5f0844d
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-09-16 15:10:19 -0300

    [ADD] req 02

---
+++ challenges/desafio2.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+  {
+    valorUnitario: {
+      $exists: false,
+    },
+  },
+  {
+    $set: {
+      valorUnitario: NumberDecimal("0.00"),
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, valorUnitario: 1 },
+);
