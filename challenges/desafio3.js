76a4a3e496d930282ce53398448cbf0b64e1deae
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-09-16 16:00:14 -0300

    requisito 3 concluido

---
+++ challenges/desafio3.js
@@ -0,0 +1,5 @@
+db.produtos.updateMany({ avaliacao: { $exists: false } },
+{ $set: { "avaliacao": NumberInt(0) } });
+db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliacao: 5 } });
+db.produtos.updateMany({ tags: "ave" }, { $inc: { avaliacao: 3 } });
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
