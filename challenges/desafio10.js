d96f4eb136204e7a2d3211501201ea28e61579d9
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-09-16 18:25:21 -0300

    corrigindo erros de eslint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,4 @@
-db.produtos.updateMany({}, { $set: { "vendasPorDia": [0, 0, 0, 0, 0, 0, 0] } });
+db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
 db.produtos.updateMany({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 db.produtos.updateMany({ tags: { $in: ["bovino", "pão"] } },
 { $inc: { "vendasPorDia.6": 120 } });
--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,2 +1,2 @@
-db.produtos.update({ nome: "Big Mac" }, { $currentDate: { "ultimaModificacao": true } });
+db.produtos.update({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } });
 db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
