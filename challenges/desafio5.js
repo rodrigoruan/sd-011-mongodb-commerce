a9de48e41baa16bd7972c15499cb0d7185aea87f
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-09-16 16:07:43 -0300

    desafio 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,5 +1,4 @@
 db.produtos.updateMany({ nome: { $not: { $eq: "McChicken" } } }, {
   $addToSet: { ingredientes: "ketchup" },
 });
-use("commerce");
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
