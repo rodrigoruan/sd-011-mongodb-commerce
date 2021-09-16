93e956809f82ca932ff40a1d40303adb60ab1bd7
Daniel Leon <daniel.radialista@gmail.com>
2021-09-16 19:48:25 -0300

    desafio 18 pronto, referenciei nos requisitos 17 e 18 sobre o default language

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -3,4 +3,4 @@ db.produtos.createIndex(
   { default_language: "portuguese" }, // da documentação: https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/
 );
 
-db.produtos.count({ $text: { $search: "frango hamburguer" } });
+db.produtos.count({ $text: { $search: "\"feito com\"" } });
