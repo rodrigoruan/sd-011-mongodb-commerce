44624251529fd7b55f6467c4cbe892e979ae846d
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-16 15:26:16 -0300

    Update desafio13.js

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,7 +1,7 @@
 // Crie uma query que faça a adição do elemento muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
 db.produtos.updateMany(
   {
-    "valoresNutricionais.percentual": { $gte: 40 }
+    "valoresNutricionais.percentual": { $gte: 40 },
   },
   {
     $push: {
