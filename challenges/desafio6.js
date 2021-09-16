7ba151cdf63d83a08b404d420b2014f8d319816b
guinhoanjos <iago.anjos@hotmail.com>
2021-09-16 17:46:44 -0300

    Create desafio6.js

---
+++ challenges/desafio6.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({ $or: [{ nome: "Big Mac" }, { nome: "Quarteirão com Queijo" }] },
+{ $push: { ingredientes: "bacon" } });
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
