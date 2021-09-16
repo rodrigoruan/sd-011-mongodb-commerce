30a2562303335d4d1dc4d4d575d486fe92317caf
nsilva90 <nikolas.silva1990@gmail.com>
2021-09-16 16:09:00 -0300

    Desafio 1 lint corrigido

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,3 +1,3 @@
-db.produtos.updateMany({}, {$set: {"criadoPor": "Ronald McDonald"}});
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
 
-db.produtos.find({}, {"nome": 1, "criadoPor": 1, "_id": 0});
+db.produtos.find({}, { nome: 1, criadoPor: 1, _id: 0 });
