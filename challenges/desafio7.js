c3a6adc9ac8719bab706a062da5a01fee582133e
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-16 16:24:39 -0300

    Ex de 7 e 8

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -2,5 +2,7 @@
 // Para isso, escreva no arquivo desafio7.js duas queries, nesta ordem:
 
 // Crie uma query que faça a remoção do item cebola em todos os sanduíches.
+db.produtos.updateMany({}, { $pull: { ingredientes: "cebola" } });
 
 // Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.find({}, { nome: true, ingredientes: true, _id: false });
--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -2,5 +2,10 @@
 // Para isso, escreva no arquivo desafio8.js duas queries, nesta ordem:
 
 // Crie uma query que faça a remoção do primeiro ingrediente no sanduíche Quarteirão com Queijo.
+db.produtos.updateMany(
+  { nome: "Quarteirão com Queijo" },
+  { $pop: { ingredientes: -1 } },
+);
 
 // Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.find({}, { nome: true, ingredientes: true, _id: false });
