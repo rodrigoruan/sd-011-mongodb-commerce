fd8ecdaac5594cee73d85bea68d9325e8bc14b58
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-16 14:22:20 -0300

    Update desafio1.js

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,9 +1,10 @@
 db.produtos.updateMany(
   {},
   {
-  $addToSet: {
-    criadoPor: "Ronald McDonald",
+    $addToSet: {
+      criadoPor: "Ronald McDonald",
+    },
   },
-})
+)
 
 db.produtos.find({}, { _id: 0, nome: 1, criadoPor: 1 });
