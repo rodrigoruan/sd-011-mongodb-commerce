36ead40bb34230862850bd7874c4ce335389d593
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-16 13:48:04 -0700

    fixing lint errors

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,5 @@
-db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0 ] } });
-db.produtos.updateMany({ nome: "Big Mac"}, { $set: { "vendasPorDia.3" : 60 } });
-db.produtos.updateMany({ $and: [ { tags: "bovino"}, { tags: "pão" }] }, { $set: { "vendasPorDia.6" :120 } });
-db.produtos.find({},{ _id:0, nome:1, vendasPorDia:1});
+db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
+db.produtos.updateMany({ nome: "Big Mac" }, { $set: { "vendasPorDia.3": 60 } });
+db.produtos.updateMany({ $and: [{ tags: "bovino" },
+ { tags: "pão" }] }, { $set: { "vendasPorDia.6": 120 } });
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,2 +1,3 @@
-db.produtos.updateMany({ $or: [{ nome: "Big Mac" }, {nome: "Quarteirão com Queijo" }]}, { $push: { ingredientes: "bacon" } });
+db.produtos.updateMany({ $or: [{ nome: "Big Mac" },
+ { nome: "Quarteirão com Queijo" }] }, { $push: { ingredientes: "bacon" } });
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
