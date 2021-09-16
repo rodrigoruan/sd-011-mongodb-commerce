58ada773c28a02695006ea1d80cdf351c4d1d229
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-16 16:14:08 -0300

    Create desafio20.js

---
+++ challenges/desafio20.js
@@ -0,0 +1,10 @@
+// Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+db.produtos.updateOne(
+  {
+    nome: "Big Mac",
+  },
+  { $unset: { curtidas: "" } },
+);
+
+// Crie uma query que retorne o nome e curtidas de todos os documentos.
+db.produtos.find({}, { _id: 0, nome: 1, curtidas: 1 })
