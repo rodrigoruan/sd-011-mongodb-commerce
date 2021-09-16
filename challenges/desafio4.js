0cd0264c93fb38a27cf3028f94a08c87dbeeffe4
Filizzola <juliofilizzola@hotmail.com>
2021-09-16 14:24:50 -0300

    desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,11 @@
+// use('commerce');
+
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: true } },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { nome: 1, _id: 0 },
+);
