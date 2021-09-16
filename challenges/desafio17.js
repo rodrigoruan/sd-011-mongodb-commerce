39d0d8318cd7fef58b098368c208aa4e2c910455
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-09-16 15:53:41 -0300

    Requisito 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,10 @@
+/** Source https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/ */
+
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
+);
+
+db.produtos.count(
+  { $text: { $search: "frango hamburguer" } },
+);
