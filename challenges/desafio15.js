deb1d82d781f3c2657b6050cde71954c5337d085
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-16 15:53:17 -0300

    update 15

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,2 +1,2 @@
 // Conte quantos produtos contêm Mc no nome, sem considerar letras maiúsculas ou minúsculas.
-db.produtos.count({ nome: { $regex: /mc/i} });
+db.produtos.count({ nome: { $regex: /mc/i } });
