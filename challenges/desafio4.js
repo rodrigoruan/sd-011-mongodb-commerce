125dcd7b0766a2550fb4d8fc941b43878e4a22e7
gab-boff <gmeninboff@gmail.com>
2021-09-16 16:50:07 -0300

    Desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,9 @@
+db.produtos.updateOne(
+    { nome: "Big Mac" },
+    { $set: { ultimaModificacao: Date() } },
+);
+
+db.produtos.find(
+    { ultimaModificacao: { $exists: true } },
+    { _id: 0, nome: 1 },
+);
