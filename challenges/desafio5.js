9c697bd1beafdcfefa1b4dd8927cc041fa07800a
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-16 16:08:23 -0300

    Ex de 5 e 6

---
+++ challenges/desafio5.js
@@ -0,0 +1,14 @@
+// 5 - Adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken, garantindo que não haja duplicidade nos ingredientes.
+// Para isso, escreva no arquivo desafio5.js duas queries, nesta ordem:
+
+// Crie uma query que adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken, garantindo que não haja duplicidade nos ingredientes.
+db.produtos.updateMany(
+  { nome: { $not: /McChicken/ } },
+  { $push: { ingredientes: "ketchup" } },
+);
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.find(
+  {},
+  { nome: true, avaliacao: true, ingredientes: true, _id: false },
+);
---
+++ challenges/desafio8.js
@@ -0,0 +1,6 @@
+// 8 - Remova o primeiro ingrediente do sanduíche Quarteirão com Queijo.
+// Para isso, escreva no arquivo desafio8.js duas queries, nesta ordem:
+
+// Crie uma query que faça a remoção do primeiro ingrediente no sanduíche Quarteirão com Queijo.
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
