14f0ae34d7aefa86caf2c2d0eadf26aca03221ef
IsaacMagno <isaacmagno98@gmail.com>
2021-09-16 18:18:22 -0300

    desafio10cor2

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,4 @@
 db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
 db.produtos.update({ nome: "Big Mac" }, { $set: { "vendasPorDia.3": 60 } });
-db.produtos.update({ tags: { $in: ["pão","bovino"] } }, { $set: { "vendasPorDia.7": 120 } });
+db.produtos.update({ tags: { $in: ["pão", "bovino"] } }, { $set: { "vendasPorDia.7": 120 } });
 db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
