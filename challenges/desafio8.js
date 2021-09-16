5f25672f6d49fc1b33286f63aaa0f9a26082f6a3
menezesrenan7 <renan_menezes07@hotmail.com>
2021-09-16 15:48:09 -0300

    Resoluçao do requsito 9.

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({ nome: "Quarteirão com Queijo" }, {$pop: { ingredientes: -1 } });
+db.produtos.updateMany({ nome: "Quarteirão com Queijo" }, { $pop: { ingredientes: -1 } });
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
