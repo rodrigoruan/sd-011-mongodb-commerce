ed4abb49ff725a72b6612121d7e3cf4f4e5459e8
jorgemeyrelles <jotaengpuc@gmail.com>
2021-09-17 13:18:56 -0300

    fix: desafio5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,14 +1,14 @@
-// db.produtos.updateMany(
-//   { nome: { $ne: "McChicken" } },
-//   { $addToSet: { ingredientes: "Ketchup" } },
-//   { upsert: true },
-// );
-
 db.produtos.updateMany(
-  { nome: { $not: { $eq: "McChicken" } } },
-  { $push: { ingredientes: { $each: ["Ketchup"] } } },
+  { nome: { $ne: "McChicken" } },
+  { $addToSet: { ingredientes: "Ketchup" } },
   { upsert: true },
 );
+
+// db.produtos.updateMany(
+//   { nome: { $not: { $eq: "McChicken" } } },
+//   { $push: { ingredientes: { $each: ["Ketchup"] } } },
+//   { upsert: true },
+// );
 
 db.produtos.find(
   {},
