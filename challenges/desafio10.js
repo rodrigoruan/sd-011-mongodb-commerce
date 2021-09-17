4a682d7b1608ca5b8ea493cc5f3086877a289d4a
Nick-713 <pereiranicolas713@gmail.com>
2021-09-17 16:51:19 -0300

    desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  {},
+  { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
+  );
+  db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $inc: { "vendasPorDia.3": 60 } },
+  );
+  db.produtos.updateMany(
+  { tags: { $in: ["bovino", "pao"] } },
+  { $inc: { "vendasPorDia.6": 120 } },
+  );
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
