edaa3c13f70464f3a572c25db71a69e0b98d3fd0
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-09-17 20:45:19 -0300

    [ADD] req 18

---
+++ challenges/desafio18.js
@@ -0,0 +1,14 @@
+// Query 1
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
+);
+
+// Query 2
+db.produtos.count(
+  {
+    $text: {
+      $search: "\"feito com\"",
+    },
+  },
+);
