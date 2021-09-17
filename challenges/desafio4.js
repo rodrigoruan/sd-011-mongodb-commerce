4857f3e3d48c1bb2b47d6b0b26744e34e2bab006
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-09-17 11:42:11 -0300

    Add Desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  {
+    $currentDate: {
+      ultimaModificacao: { $type: "date" },
+    },
+  },
+);
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: 1, _id: 0 });
