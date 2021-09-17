46513992dc2c519d449f54a3816ef3de104b0748
Nick-713 <pereiranicolas713@gmail.com>
2021-09-17 17:00:24 -0300

    desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany(
+  {},
+  { $push: { valoresNutricionais: {
+    $each: [],
+    $sort: { percentual: -1 },
+  } } },
+);
+db.produtos.find({}, {nome: 1, _id: 0, valoresNutricionais: 1 });
