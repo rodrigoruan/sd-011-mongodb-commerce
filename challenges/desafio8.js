4ca707ee39ce6482447062399fb5ec529260debe
Luan Xavier <luantenorio3@gmail.com>
2021-09-17 11:34:37 -0300

    Requisito 8: ajustar indentaçao

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,9 +1,9 @@
 db.produtos.updateOne(
-    { nome: "Quarteirão com Queijo" },
-    { $pop: { ingredientes: -1 } },
-  );
+  { nome: "Quarteirão com Queijo" },
+  { $pop: { ingredientes: -1 } },
+);
   
-  db.produtos.find(
-    {},
-    { _id: 0, nome: 1, ingredientes: 1 },
-  );
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, ingredientes: 1 },
+);
