30953887f26bb06234128308131dbf01c3a4277f
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-09-17 11:31:59 -0300

    Add Desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,28 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      avaliacao: NumberInt(0),
+    },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["bovino"] } },
+  {
+    $inc: {
+      avaliacao: 5,
+    },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["ave"] } },
+  {
+    $inc: {
+      avaliacao: 3,
+    },
+  },
+);
+
+db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });
