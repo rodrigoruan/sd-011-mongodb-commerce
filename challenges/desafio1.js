4ce06ae68c24f418f2e79c20f3347e07df0cc53e
AlexSmith7192 <alexsmith7192@gmail.com>
2021-09-17 14:49:19 -0300

    1 - fix nome

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -3,4 +3,4 @@ db.produtos.updateMany(
   { $set: { criadoPor: "Ronald McDonald" } },
 );
 
-db.produtos.find({}, { _id: 0, criadoPor: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, criadoPor: 1 });
