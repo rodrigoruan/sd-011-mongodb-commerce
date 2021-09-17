23b59512ce20f7910ca62c9c39e2c18591cc65eb
Nick-713 <pereiranicolas713@gmail.com>
2021-09-17 16:14:52 -0300

    desafio2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,10 +1,10 @@
 db.produtos.updateMany(
-    {},
-    {$set: {valorUnitario: NumberDecimal("0.00")}},
+  {},
+  {$set: {valorUnitario: NumberDecimal("0.00")}},
   );
   
 db.produtos.find({}, {
-    nome: 1,
-    valorUnitario: 1,
-    _id: 0,
+  nome: 1,
+  valorUnitario: 1,
+  _id: 0,
   });
