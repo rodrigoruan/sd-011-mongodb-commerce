026b97712821a293ca1eac21feb71cb2363226b0
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-09-17 20:15:55 -0300

    [ADD] req 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,26 @@
+// Query 1
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: { $gt: 20, $lt: 40 },
+      },
+    },
+  },
+  {
+    $push: {
+      tags: "contém sódio",
+    },
+  },
+);
+
+// Query 2
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    tags: 1,
+  },
+);
