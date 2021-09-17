108eb45c34527dfc136563897f21301e1ab21042
OldRoge <rogeriomrjr@icloud.com>
2021-09-17 16:53:22 -0300

    Done requirement 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,33 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      vendasPorDia: [0, 0, 0, 0, 0, 0, 0],
+    },
+  },
+);
+
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $inc: {
+      "vendasPorDia.3": 60,
+    },
+  },
+);
+
+db.produtos.updateMany(
+  {
+    $or: [
+      { tags: "bovino" },
+      { tags: "pão" },
+    ],
+  },
+  {
+    $inc: {
+      "vendasPorDia.6": 120,
+    },
+  },
+);
+
+db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
