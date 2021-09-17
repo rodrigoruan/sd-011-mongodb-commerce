61dd75fe2385f137c598c0a1c279d445f0304371
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-17 19:42:06 -0300

    feito 13 e 14.

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -3,8 +3,8 @@
 
 // Crie uma query que faça a adição do elemento muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
 db.produtos.updateMany(
-  { "valoresNutricionais.percentual": { $gte: 40 } },
-  { $push: { tags: { $each: ["muito sódio"] } } },
+  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
+  { $addToSet: { tags: { $each: ["muito sódio"] } } },
 );
-// Crie uma query que retorne o nome e tags de todos os documentos.
+// Crie uma query que retorne o nome e tags de todos os documentos. 
 db.produtos.find({}, { nome: true, tags: true, _id: false });
--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,6 +1,11 @@
 // 14 - Adicione o elemento contém sódio ao final do array tags nos produtos em que o percentual de sódio seja maior do que 20 e menor do que 40.
 // Para isso, escreva no arquivo desafio14.js duas queries, nesta ordem:
 
-// Crie uma query que faça a adição do elemento contém sódio ao final do array tags nos produtos em que o percentual de sódio seja maior do que 20 e menor do que 40.
+// Crie uma query que faça a adição do elemento "contém sódio" ao final do array tags nos produtos em que o percentual de sódio seja maior do que 20 e menor do que 40.
+db.produtos.updateMany(
+  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } } },
+  { $addToSet: { tags: { $each: ["contém sódio"] } } },
+);
 
-// Crie uma query que retorne o nome e tags de todos os documentos.
+// Crie uma query que retorne o nome e tags de todos os documentos. 
+db.produtos.find({}, { nome: true, tags: true, _id: false });
