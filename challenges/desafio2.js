bf1a0c35feb8306500d113eb79c53e27992767fd
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-09-17 11:20:31 -0300

    Add Desafio 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+  { valorUnitario: { $exists: false } },
+  {
+    $set: {
+      valorUnitario: NumberDecimal("0.00"),
+    },
+  },
+);
+
+db.produtos.find({}, { nome: 1, valorUnitario: 1, _id: 0 });
