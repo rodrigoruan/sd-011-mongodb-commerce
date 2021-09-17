bf6a176de8db0c86098b6f8af11a2c6d8a21188a
jorgemeyrelles <jotaengpuc@gmail.com>
2021-09-17 19:56:19 -0300

    fix: desafios 5 e 22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,4 +1,4 @@
 db.produtos.find(
   { vendidos: { $mod: [5, 0] } },
-  { _id: 0, nome: 1, vendidos: 1 }
+  { _id: 0, nome: 1, vendidos: 1 },
 );
--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -7,4 +7,3 @@ db.produtos.find(
   {},
   { _id: 0, nome: 1, ingredientes: 1 },
 );
-  
