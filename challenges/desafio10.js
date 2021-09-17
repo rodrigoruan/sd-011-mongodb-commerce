f0d095881b511787f4b535b356d22440cc2d6f49
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-17 19:17:37 -0300

    Ajsute no 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -15,7 +15,7 @@ db.produtos.updateMany({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 // Crie uma query que incremente as vendas de todos os sanduíches de carne do tipo bovino e pão aos sábados em 120.
 db.produtos.updateMany(
   { tags: ["bovino", "pão"] },
-  { $inc: { "vendasPorDia.6": 60 } },
+  { $inc: { "vendasPorDia.6": 120 } },
 );
 
 // Crie uma query que retorne o nome e vendasPorDia de todos os documentos.
