1ad461e2bb1ba8881866157776c9c30cb9ed500e
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-09-16 22:46:35 -0300

    comitão bomba bombástico que ninguém segura

---
+++ challenges/desafio13.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({ $and: [{ "valoresNutricionais.tipo": "sódio" }, 
+{ "valoresNutricionais.percentual": { $gte: 40 } }] }, { $push: { tags: "muito sódio" } });
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
