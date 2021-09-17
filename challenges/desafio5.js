96ceab225e31f458da4f5716615f1c29316a7bce
jorgemeyrelles <jotaengpuc@gmail.com>
2021-09-17 19:28:51 -0300

    fix: desafios 5 e 20

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -3,13 +3,13 @@ db.produtos.updateOne(
   { $addToSet: { ingredientes: "Ketchup" } },
 );
 
-// db.produtos.updateOne(
-//   { nome: { $not: { $eq: "McChicken" } } },
-//   { $push: { ingredientes: { $each: ["Ketchup"] } } },
-//   { upsert: true },
-// );
-
 db.produtos.find(
   {},
   { _id: 0, nome: 1, ingredientes: 1 },
-);
+  );
+  
+  // db.produtos.updateOne(
+  //   { nome: { $not: { $eq: "McChicken" } } },
+  //   { $push: { ingredientes: { $each: ["Ketchup"] } } },
+  //   { upsert: true },
+  // );
