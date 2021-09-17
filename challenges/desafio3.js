fe9cf9ea9db9da2925de39c0584c50da9d6eba41
Nick-713 <pereiranicolas713@gmail.com>
2021-09-17 16:21:36 -0300

    desafioo3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -2,3 +2,15 @@ db.produtos.updateMany(
   {},
   { $set: { avaliacao: NumberInt("0") } },
 );
+
+db.produtos.updateMany(
+  { tags: { $all: ["bovino"] } },
+  { $inc: { avaliacao: NumberInt(5) } },
+);
+
+db.produtos.updateMany(
+  { tags: "ave" },
+  { $inc: { avaliacao: NumberInt(3) } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
