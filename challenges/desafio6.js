f51c1fd50370b9b5272cd93977aca984c417853e
IsleneGomes <islenegomes@gmail.com>
2021-09-18 04:14:40 -0300

    Desafio 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany(
+  { $or: [{ nome: "Big Mac" }, { nome: "Quarteirão com Queijo" }] },
+  { $push: { ingredientes: "bacon" } },
+);
+
+db.produtos.find(
+  {},
+  { _id: false, nome: true, ingredientes: true },
+); 
