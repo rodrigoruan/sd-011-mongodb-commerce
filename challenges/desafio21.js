7939a2ab6dd991643f404853ffafa2c0e586a073
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-18 14:38:56 -0300

    Feito ate o 22.

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,2 +1,10 @@
 /* 
 21 - Retorne o nome dos sanduíches em que o número de curtidas é maior que o número de sanduíches vendidos. */
+db.produtos.find(
+  {
+    $expr: {
+      $gt: ["$curtidas", "$vendidos"],
+    },
+  },
+  { nome: true, _id: false },
+);
--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,2 +1,10 @@
 /* 
 22 - Retorne o nome e a quantidade de vendas (vendidos) dos sanduíches em que o número de vendas é múltiplo de 5. */
+db.produtos.find(
+  {
+    vendidos: {
+      $mod: [5, 0],
+    },
+  },
+  { nome: true, vendidos: true, _id: false },
+);
