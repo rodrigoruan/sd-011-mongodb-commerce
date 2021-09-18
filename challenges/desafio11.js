b4b84dda692bc5b545cd5ae3eb8686f97bed93b7
Vinicius <viniciusfod@gmail.com>
2021-09-18 20:40:51 -0300

    req 11-fix

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,3 +1,3 @@
-db.products.updateMany({ $push: { tags: { $each: ["combo", "tasty"] }}}, { sort: { tags: 1 }})
+db.products.updateMany({ $push: { tags: { $each: ["combo", "tasty"] } }}, { sort: { tags: 1 } });
 
-db.products.find({}, { _id: 0, name: 1, tags: 1 })
+db.products.find({}, { _id: 0, name: 1, tags: 1 });
