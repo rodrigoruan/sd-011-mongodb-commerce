957445637eddb2dc310eed4d9649b68e73bf6eb3
Maran <maranmorimoto@gmail.com>
2021-09-18 14:15:29 -0300

    reqs 15, 16, 17 and 18 done.

---
+++ challenges/desafio17.js
@@ -0,0 +1,16 @@
+// 17 - Conte quantos documentos contêm as palavras `frango`
+// ou `hamburguer` utilizando o operador `$text`. Para isso,
+//  escreva no arquivo `desafio17.js` duas queries, **nesta 
+//  ordem**:
+
+// 1. Crie uma query que faça a criação de um índice do tipo
+// `text` no campo `descricao` com o idioma padrão `portuguese`.
+db.produtos.createIndex(
+    { descricao: "text" },
+    { default_language: "portuguese" },
+);
+
+// 2. Crie uma query que retorne a quantidade de documentos
+// que contêm as palavras `frango` ou `hamburguer` utilizando 
+// o operador `$text`.
+db.produtos.count({ $text: { $search: "frango hamburguer" } });
---
+++ challenges/desafio18.js
@@ -0,0 +1,17 @@
+// Conte quantos documentos contêm a **expressão** 
+// `feito com` utilizando o operador `$text`. Para 
+// isso, escreva no arquivo `desafio18.js` duas queries,
+//  **nesta ordem**:
+
+// Crie uma query que faça a criação de um índice do tipo 
+// `text` no campo `descricao` com o idioma padrão 
+// `portuguese`.
+db.produtos.createIndex(
+    { descricao: "text" },
+    { default_language: "portuguese" },
+);
+
+// Crie uma query que retorne a quantidade de documentos
+// que contêm a **expressão** `feito com` utilizando o
+// operador `$text`.
+db.produtos.count({ $text: { $search: "\"feito com\"" } });
