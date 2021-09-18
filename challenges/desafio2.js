d3b6cd2cc5d6068a64c13ff463d636f7fad19cff
Vinicius <viniciusfod@gmail.com>
2021-09-18 17:03:31 -0300

    fix idetetacao

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,4 +1,4 @@
 db.produtos.updateMany({},
-     { $set: { valorUnitario: 0.00 } } )
+     { $set: { valorUnitario: 0.00 } });
 
-db.produtos.find( {}, { _id: 0, nome: 1, valorUnitario: 1 } )
+db.produtos.find({}, { _id: 0, nome: 1, valorUnitario: 1 });
