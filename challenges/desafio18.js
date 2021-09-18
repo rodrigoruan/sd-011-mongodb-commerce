89ca09efb9d19ef70a7794716cf72130641a961b
IsaacMagno <isaacmagno98@gmail.com>
2021-09-18 14:52:08 -0300

    some corretions

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,5 +1,5 @@
 db.produtos.createIndex(
   { descricao: "text" },
-  { default_language: "portuguese" }
+  { default_language: "portuguese" },
   );
 db.produtos.find({ $text: { $search: "\"feito com\"" } }).count();
