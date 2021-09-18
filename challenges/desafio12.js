d13a7fcac7c084c69bd44dc869fa58410b945dfe
IsleneGomes <islenegomes@gmail.com>
2021-09-18 04:33:59 -0300

    Desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany(
+  {},
+  { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } },
+);
+
+db.produtos.find(
+  {},
+  { _id: false, nome: true, valoresNutricionais: true },
+); 
