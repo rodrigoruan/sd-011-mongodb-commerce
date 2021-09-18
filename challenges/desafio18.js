2a987764b84164fdeee010ff17f03638a51b5f83
OldRoge <rogeriomrjr@icloud.com>
2021-09-17 21:08:30 -0300

    Done requirement 18

---
+++ challenges/desafio18.js
@@ -0,0 +1,16 @@
+db.produtos.createIndex(
+  {
+    descricao: "text",
+  },
+  {
+    default_language: "pt",
+  },
+);
+
+db.produtos.countDocuments(
+  {
+    $text: {
+      $search: "\"feito com\"",
+    },
+  },
+);
