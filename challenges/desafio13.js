7b6f6e29aad64ea34682e08e73fe725859c6189e
IsleneGomes <islenegomes@gmail.com>
2021-09-18 04:41:35 -0300

    Desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany(
+  { valoresNutricionais: { $elemMatch: {
+    tipo: "sódio",
+    percentual: { $gte: 40 },
+  } } },
+  { $push: { tags: "muito sódio" } },
+);
+
+db.produtos.find({}, { _id: false, nome: true, tags: true });
