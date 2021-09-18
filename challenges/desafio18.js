e876be82994492c9b4fe242359c3924f05948fd3
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-18 14:19:20 -0300

    Feito ate o 19.

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,16 +1,8 @@
-/* 
-18 - Conte quantos documentos contêm a expressão feito com utilizando o operador $text.
-Para isso, escreva no arquivo desafio18.js duas queries, nesta ordem:
+// 18 - Conte quantos documentos contêm a expressão feito com utilizando o operador $text.
+// Para isso, escreva no arquivo desafio18.js duas queries, nesta ordem:
 
-Crie uma query que faça a criação de um índice do tipo text no campo descricao com o idioma padrão portuguese.
+// Crie uma query que faça a criação de um índice do tipo text no campo descricao com o idioma padrão portuguese.
+db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
 
-Crie uma query que retorne a quantidade de documentos que contêm a expressão feito com utilizando o operador $text.
-*/
-
-// $size
-// createIndex $text
-// $search
-// $unset
-// $expr
-// { $expr: { $gt: ["$curtidas", "$vendidos"]
-// $mod
+// Crie uma query que retorne a quantidade de documentos que contêm a expressão feito com utilizando o operador $text.
+db.produtos.countDocuments({ $text: { $search: "\"feito com\"" } });
--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,7 +1,7 @@
-/* 
-19 - Renomeie o campo descricao para descricaoSite em todos os documentos.
-Para isso, escreva no arquivo desafio19.js duas queries, nesta ordem:
+// 19 - Renomeie o campo descricao para descricaoSite em todos os documentos.
+// Para isso, escreva no arquivo desafio19.js duas queries, nesta ordem:
+// Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
+db.produtos.updateMany({}, { $rename: { descricao: "descricaoSite" } });
 
-Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
-
-Crie uma query que retorne o nome, descricao e descricaoSite de todos os documentos. */
+// Crie uma query que retorne o nome, descricao e descricaoSite de todos os documentos.
+db.produtos.find({}, { nome: true, descricao: true, descricaoSite: true, _id: false });
