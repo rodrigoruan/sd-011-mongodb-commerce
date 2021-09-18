e08d69cb12b2eb9b19f70206f1ac1aeb7e38c43e
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-18 14:26:41 -0300

    Feito ate o 20.

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,7 +1,8 @@
-/* 
-20 - Remova o campo curtidas do item Big Mac.
-Para isso, escreva no arquivo desafio20.js duas queries, nesta ordem:
+// 20 - Remova o campo curtidas do item Big Mac.
+// Para isso, escreva no arquivo desafio20.js duas queries, nesta ordem:
 
-Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+// Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+db.produtos.updateOne({ nome: "Big Mac" }, { $unset: { curtidas: "" } });
 
-Crie uma query que retorne o nome e curtidas de todos os documentos. */
+// Crie uma query que retorne o nome e curtidas de todos os documentos. 
+db.produtos.find({}, { nome: true, curtidas: true, _id: false });
