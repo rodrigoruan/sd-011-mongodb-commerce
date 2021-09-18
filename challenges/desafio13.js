9cc94a29f38e2d7ddc90e5ea8f2681857467721c
LeoLacana <wilson17414@gmail.com>
2021-09-18 03:22:02 -0300

    requisito_13_feito

---
+++ challenges/desafio13.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany(
+    { $and: [
+            { "valoresNutricionais.2.tipo": "sódio" },
+            { "valoresNutricionais.2.percentual": { $gte: 40 } },
+    ] },
+    { $push: { tags: "muito sódio" } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
