0a181d38f697b9e0002fb0879938662d2e5cd99f
Aline <aline.melryaa@gmail.com>
2021-09-18 08:37:35 -0400

    req:4-fix

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,4 @@
 db.produtos.updateMany({ nome: "Big Mac" },
 { $currentDate: { ultimaModificacao: { $type: "date" } } });
-db.produtos.find({ ultimaModificacao: { $exists: 1 } });
+db.produtos.find({ ultimaModificacao: { $exists: 1 } },
+{ _id: 0, nome: 1 });
