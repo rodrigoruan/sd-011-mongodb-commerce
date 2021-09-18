18bef9111de175262306dce5d626b2a68bdcfb49
Vinicius <viniciusfod@gmail.com>
2021-09-18 20:54:56 -0300

    req 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany({
+     "valoresNutricionais.tipo": { $eq: "sódio" }, "valoresNutricionais.percentual": { $gte: 40 },   
+    }, 
+    { 
+        $push: { tags: "muito sódio" }
+    });
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
