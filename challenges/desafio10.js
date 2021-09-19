b4513f1f24e8c41951e6323f828d9b723ecb75d6
lucasSCsantos <luc.cristovam10@gmail.com>
2021-09-19 15:22:19 -0300

    11-14

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,4 +2,15 @@ db.produtos.updateMany(
 {},
 { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
 );
-db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
+db.produtos.updateMany(
+{ nome: "Big Mac" },
+{ $inc: { "vendasPorDia.3": 60 } },
+);
+db.produtos.updateMany(
+{ $and: [
+{ tags: { $elemMatch: { $eq: "bovino" } } },
+{ tags: { $elemMatch: { $eq: "pão" } } },
+] },
+{ $inc: { "vendasPorDia.6": 120 } },
+);
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
